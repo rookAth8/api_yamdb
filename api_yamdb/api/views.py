08b367ad2595f86eda3d4aa82f74cd305c5d5d93
Krotov Ilya <i.a.krotovv@gmail.com>
2022-08-17 10:09:34 +0200

    1_ревью_третья_часть

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,21 +1,21 @@
+from django.db.models import Avg
+from django.db.utils import IntegrityError
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
-from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.exceptions import ParseError, PermissionDenied
+from rest_framework.decorators import action
+from rest_framework.exceptions import ParseError
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import (LimitOffsetPagination,
                                        PageNumberPagination)
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 from .filters import TitleFilter
 from .mixins import ListCreateDestroyViewSet
-from .permissions import IsAdminOrReadOnly, IsRoleAdmin
+from .permissions import (IsAdminOrReadOnly, IsRoleAdmin,
+                          ReviewCommentCustomPermission)
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer,
                           TitleGetSerializer, TitlePostSerializer,
@@ -43,7 +43,7 @@ class GenreViewSet(ListCreateDestroyViewSet):
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
+    queryset = Title.objects.all().annotate(rating=Avg('reviews__score'))
     permission_classes = (IsAdminOrReadOnly,)
     pagination_class = LimitOffsetPagination
     filter_backends = (DjangoFilterBackend,)
@@ -93,71 +93,36 @@ class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = [IsAuthenticatedOrReadOnly | IsAdminOrReadOnly]
+    permission_classes = [ReviewCommentCustomPermission]
 
     def get_queryset(self):
         title_id = self.kwargs['titles_id']
-        title = get_object_or_404(Title, id=title_id)
-        return Review.objects.filter(title=title)
+        return Review.objects.filter(title__id=title_id)
 
     def perform_create(self, serializer):
-        title_id = self.kwargs['titles_id']
-        title = get_object_or_404(Title, id=title_id)
-        author = self.request.user
-        if Review.objects.filter(
-            author=author
-        ).filter(title=title).exists():
-            raise ParseError(
-                'Можно оставить только один отзыв'
+        try:
+            title_id = self.kwargs['titles_id']
+            title = get_object_or_404(Title, id=title_id)
+            author = self.request.user
+            serializer.save(
+                author=author,
+                title=title,
             )
-        serializer.save(
-            author=author,
-            title=title,
-        )
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужих отзывов запрещено')
-        return super().perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        user = self.request.user
-        if instance.author == self.request.user or (
-            user.is_admin or user.is_superuser or user.is_moderator
-        ):
-            return super().perform_destroy(instance)
-        raise PermissionDenied('Нет прав на удаление')
+        except IntegrityError:
+            raise ParseError('Можно оставить только один отзыв')
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = [IsAuthenticatedOrReadOnly | IsAdminOrReadOnly]
+    permission_classes = [ReviewCommentCustomPermission]
 
     def get_queryset(self):
-        title_id = self.kwargs['titles_id']
-        title = get_object_or_404(Title, id=title_id)
         review_id = self.kwargs['review_id']
-        review = get_object_or_404(Review, id=review_id, title=title)
-        return Comment.objects.filter(review=review)
+        return Comment.objects.filter(review__id=review_id)
 
     def perform_create(self, serializer):
-        title_id = self.kwargs['titles_id']
-        title = get_object_or_404(Title, id=title_id)
         review_id = self.kwargs['review_id']
-        review = get_object_or_404(Review, id=review_id, title=title)
+        review = get_object_or_404(Review, id=review_id)
         serializer.save(author=self.request.user, review=review)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужих комментариев запрещено')
-        return super().perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        user = self.request.user
-        if instance.author == self.request.user or (
-            user.is_admin or user.is_superuser or user.is_moderator
-        ):
-            return super().perform_destroy(instance)
-        raise PermissionDenied('Нет прав на удаление')
